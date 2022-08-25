72bd3363ccee96aea961786c3214aa5db165aa34
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-08-25 13:26:13 +0300

    Add mirror

---
+++ Exercises/mirror.py
@@ -0,0 +1,10 @@
+link = "https://www.codewars.com/kata/581331293788bc1702001fa6"
+
+
+def mirror(text):
+    reverse_words = [i[::-1] for i in text.split()]
+    mirror_width = len(max(reverse_words, key=len)) + 4
+    word = '*' * mirror_width + "\n"
+    for i in reverse_words:
+        word += "*" + " " + i + " " * (mirror_width - len(i) - 3) + "*" + "\n"
+    return word + '*' * mirror_width
