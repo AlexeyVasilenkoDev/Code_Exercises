f2db530828ca27c0405959dfc19edfc2911ab4ab
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-10-06 10:37:30 +0300

    Fix Unique words generator

--- Exercises/iterators&generators/unique_words_generator.py
+++ Exercises/iterators&generators/unique_words_generator.py
@@ -2,13 +2,19 @@ from random_word import RandomWords
 
 
 def unique_words_generator(number_of_words):
+    words_limit = 10000
+    list_of_words = ['a', 'b', 'c', 'd', 'e']
+    assert number_of_words <= words_limit
     word = RandomWords()
     list_of_words = set()
     while len(list_of_words) < number_of_words:
         try:
             new_word = word.get_random_word()
-            list_of_words.add(new_word)
-            yield new_word
+            if new_word in list_of_words:
+                new_word = word.get_random_word()
+            else:
+                list_of_words.add(new_word)
+                yield new_word
         except StopIteration as ex:
             raise ex
 
