ed1c0bf1fcfa4ad5a094707f83dffa16afe39366
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-11-07 22:00:33 +0200

    Finish async homework

--- Exercises/async/coroutines.py
+++ Exercises/async/coroutines.py
@@ -1,21 +1,41 @@
 import asyncio
 import inspect
 import pprint
+
 import random
 
 import requests
 from geonamescache import GeonamesCache
 
 
+async def call_friends():
+    print("Starting coroutine!")
+    friends = random.choice([True, False])
+    print(friends)
+    if friends:
+        await gather_worker()
+    else:
+        print("Sorry, you don`t have any friends to travel with!")
+        await end_process(event_loop)
+
+
+async def gather_worker():
+    result = await asyncio.gather(
+        get_weather_podcast(fut, "2022-12-31"),
+        choose_suitable_clothes(fut),
+        end_process(event_loop)
+    )
+    pprint.pprint(result)
+
+
 async def get_random_city():
     gc = GeonamesCache()
     city = random.choice(list(gc.get_cities().items()))[1]["name"]
-    # print(city)
+    print(city)
     return city
 
 
 async def get_weather_podcast(future, date):
-    print("Starting coroutine!")
     while True:
         city = await get_random_city()
         weather_forecast = requests.get(
@@ -23,26 +43,19 @@ async def get_weather_podcast(future, date):
             f"key=f5ac2565c90745acb59181224220611&"
             f"q={city}&"
             f"dt={date}").json()
-        if list(dict(weather_forecast).keys())[0] == "location" and city == (dict(weather_forecast))["location"][
-            "name"]:
-            # print(weather_forecast)
+        if list(dict(weather_forecast).keys())[0] == "location" and \
+                city == (dict(weather_forecast))["location"]["name"]:
             print(f"{inspect.stack()[0][3]} function finished")
             weather_dict = {"City": city,
                             "Date": weather_forecast["forecast"]["forecastday"][0]["hour"][5]["time"],
                             "Degrees": weather_forecast["forecast"]["forecastday"][0]["day"]["avgtemp_c"],
-                            # I know how bad it looks like, but it still works
                             "Rain": weather_forecast["forecast"]["forecastday"][0]["hour"][5]["will_it_rain"]}
-            # pprint.pprint(weather_dict)
+            # I know how bad it looks like, but it still works
             future.set_result(weather_dict)
             return weather_dict, date
 
 
 async def choose_suitable_clothes(future):
-    clothes_dict = {
-        "Hot": "T-shirt and shirts",
-        "Medium": "Hoodie and jeans",
-        "Cold": "Jacket and warm pants"
-    }
     result = await future
     print(result)
     if result['Degrees'] > 20:
@@ -55,22 +68,20 @@ async def choose_suitable_clothes(future):
                      f"{'and don`t forget to take an umbrella ' if result['Rain'] else ''}" \
                      f"if you want to visit {result['City']} on {result['Date']}"
     print(clothes_choice)
-    # pprint.pprint(result)
     print(f"{inspect.stack()[0][3]} function finished")
+    return clothes_choice
 
 
 async def end_process(loop):
     print(f"{inspect.stack()[0][3]} function finished")
-    loop.stop()
     print("Event loop ended!")
+    loop.stop()
 
 
 fut = asyncio.Future()
 event_loop = asyncio.get_event_loop()
 
-event_loop.create_task(get_weather_podcast(fut, "2022-12-31"))
-event_loop.create_task(choose_suitable_clothes(fut))
-event_loop.create_task(end_process(event_loop))
+event_loop.create_task(call_friends())
 
 event_loop.run_forever()
 event_loop.close()
